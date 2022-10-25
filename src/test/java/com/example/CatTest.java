b08d7c8ce0b1a82899baa4118bac717daf670d10
DodGi97 <gudimivan86@gmail.com>
2022-10-25 20:00:24 +0600

    create test for Cat, Lion, Feline and refactor class Lion

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+   @Mock
+   Feline feline;
+   Cat cat;
+
+   @Before
+   public void init(){
+       cat = new Cat(feline);
+   }
+
+    @Test
+    public void shouldReturnMeow() {
+        Cat cat = new Cat(feline);
+        assertEquals("Кошка не говорит 'Мяу'", cat.getSound(), "Мяу");
+    }
+
+    @Test
+    public void shouldReturnMeowNegative() {
+        Cat cat = new Cat(feline);
+        Assert.assertNotEquals("Кошка не говорит 'Гав'", cat.getSound(), "Гав");
+    }
+
+    @Test
+    public void getReturnsListOfPredatorFood(){
+        try {
+            Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+            Assert.assertEquals("Ожидался рацион хищника", List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
